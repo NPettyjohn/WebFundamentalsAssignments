1cdd3c92bf52908f8d8588714eadc7a3bcb59269
npettyjohn <nicholas.pettyjohn@gmail.com>
2017-07-16 10:04:28 -0700

    added 'I do Declare.js'

---
+++ Javascript/I do Declare.js
@@ -0,0 +1,31 @@
+
+// I do Declare Assignment
+
+var intNum1 = 150000000;
+var intNum2 = 1987.30;
+var intNum3 = .45;
+var intNum4 = 60.425;
+
+var strMyString1 = 'Test string 1';
+var strMyString2 = 'Test string 2';
+var strMyString3 = 'Test string 3';
+
+var blnIsFalse = false;
+var blnIsTrue = true;
+
+var varVariable1;
+
+// Print all of the numbers.
+  console.log('The first number: ', intNum1);
+  console.log('The second number: ', intNum2);
+  console.log('The third number: ', intNum3);
+  console.log('The fourth number: ', intNum4);
+
+// Print all of the strings.
+  console.log('The first string: ', strMyString1);
+  console.log('The second string: ', strMyString2);
+  console.log('The third string: ', strMyString3);
+
+// Print the booleans.
+  console.log("First boolean: ", blnIsFalse);
+  console.log("Second boolean: ", blnIsTrue);
